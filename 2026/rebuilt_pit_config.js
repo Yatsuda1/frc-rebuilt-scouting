d80a03d47fb046f10598e38e6081260a98599b21
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2026-01-16 16:41:21 -0500

    Update rebuilt_pit_config.js

--- 2026/rebuilt_pit_config.js
+++ 2026/rebuilt_pit_config.js
@@ -23,7 +23,7 @@ var config_data = `
         },
         {
             "name": "Drivetrain",
-            "code": "DRV",
+            "code": "DVT",
             "type": "radio",
             "choices": {
                 "s": "Swerve<br>",
