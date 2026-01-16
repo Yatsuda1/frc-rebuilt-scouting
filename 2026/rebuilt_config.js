6a7d60e47dae615a2b9c338e551ce15d8c0f4b55
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2026-01-16 13:58:33 -0500

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -60,7 +60,6 @@ var config_data = `
       "type": "clickable_image",
       "filename": "2026/field_image.png",
       "clickRestriction": "one",
-      "allowableResponses": "6 7 18 19 30 31 42 43 54 55 66 67",
       "shape": "circle 5 black red true"
     }
   ],
