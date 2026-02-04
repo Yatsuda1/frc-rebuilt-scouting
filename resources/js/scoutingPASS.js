b34f3fa08b50b427d379803d52becb3aafab755c
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2026-02-04 12:25:58 -0500

    Update scoutingPASS.js

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -1,7 +1,7 @@
 // ScoutingPASS.js
 //
 // The guts of the ScountingPASS application
-// Written by Team 2451 - PWNAGE, modified by Team 5505 Volts Squared
+// Written by Team 2451 - PWNAGE, modified by Team 3603, The Cyber Coyotes
 
 document.addEventListener("touchstart", startTouch, false);
 document.addEventListener("touchend", moveTouch, false);
