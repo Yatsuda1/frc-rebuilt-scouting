88150c025bd3f0f0294de2b1f3169d03933081da
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2026-04-06 14:11:15 -0400

    Change defaultValue from '2026mimus' to '2026mibig'

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -16,7 +16,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026mimus",
+      "defaultValue": "2026mibig",
       "required": "true"
     },
     { "name": "Match Level",
