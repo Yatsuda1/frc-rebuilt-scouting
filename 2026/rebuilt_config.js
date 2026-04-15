86f07677e87bb0c4dc67d408531233073a6e1f4d
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2026-04-15 10:35:30 -0400

    Change defaultValue from '2026mibig' to '2026micmp4'

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -16,7 +16,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026mibig",
+      "defaultValue": "2026micmp4",
       "required": "true"
     },
     { "name": "Match Level",
